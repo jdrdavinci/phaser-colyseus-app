a42fade6611d1735f7f6d2a076251b8c9636fdbe
jdrdavinci <jderuijter@davinci.nl>
2021-02-13 23:39:13 +0100

    setup colyseus client server

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,10 +1,11 @@
 import Phaser from 'phaser'
+import 'regenerator-runtime/runtime'
 
 import HelloWorldScene from './scenes/HelloWorldScene'
 
 const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
-	width: 800,
+	width: 700,
 	height: 600,
 	physics: {
 		default: 'arcade',
